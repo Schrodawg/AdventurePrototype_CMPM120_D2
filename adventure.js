cb4b14129bf1ede55db64b0805d2eb68d0bff9ca
Schrodawg <cjschroe@ucsc.edu>
2023-05-12 14:49:20 -0700

    menu and 1st scene

--- adventure.js
+++ adventure.js
@@ -16,10 +16,10 @@ class AdventureScene extends Phaser.Scene {
         this.h = this.game.config.height;
         this.s = this.game.config.width * 0.01;
 
-        this.cameras.main.setBackgroundColor('#444');
+        this.cameras.main.setBackgroundColor('#0');
         this.cameras.main.fadeIn(this.transitionDuration, 0, 0, 0);
 
-        this.add.rectangle(this.w * 0.75, 0, this.w * 0.25, this.h).setOrigin(0, 0).setFillStyle(0);
+        // this.add.rectangle(this.w * 0.75, 0, this.w * 0.25, this.h).setOrigin(0, 0).setFillStyle(0);
         this.add.text(this.w * 0.75 + this.s, this.s)
             .setText(this.name)
             .setStyle({ fontSize: `${3 * this.s}px` })
--- game.js
+++ game.js
@@ -3,8 +3,19 @@ class Demo1 extends AdventureScene {
         super("demo1", "First Room");
     }
 
-    onEnter() {
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('bg1', 'scene1bg.jpg');
+        this.load.image('guy', 'guy.png');
+    }
 
+    onEnter() {
+        //BG
+        let bg1 = this.add.image(this.w*.25, this.h*.5, 'bg1');
+        //Player
+        let player = this.add.image(-100, this.h*.75,'guy')
+        player.setScale(.35);
+        //
         let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
             .setFontSize(this.s * 2)
             .setInteractive()
@@ -57,7 +68,14 @@ class Demo1 extends AdventureScene {
                     this.gotoScene('demo2');
                 }
             })
-
+            //animation
+            this.tweens.add({
+                targets: player,
+                x: this.w*.25,
+                delay: 500,
+                duration: 5000,
+                ease: 'Linear',
+            })
     }
 }
 
@@ -96,12 +114,45 @@ class Intro extends Phaser.Scene {
     constructor() {
         super('intro')
     }
+
+    preload(){
+        this.load.path = './assets/';
+        this.load.image('plane', 'plane.png');
+    }
+
     create() {
-        this.add.text(50,50, "Adventure awaits!").setFontSize(50);
-        this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
+        //BG
+        let menuBG = this.add.graphics();
+        menuBG.fillStyle(0x6dbbf7, 1);
+        menuBG.fillRect(0, 0, 1920, 1080);
+        //Text
+        let menuText = this.add.text(1920*0.125,1080*.25, "That Time I Got Stranded\non an Island After a Plane\nCrash and Was Reborn as a\nFrog",
+        {
+            font: "Courier",
+            color: "#ffffff",
+            align: 'center',
+        }
+        ).setFontSize(90);
+        let clickText = this.add.text(1920*.425,1080*.75, "Click anywhere to begin.").setFontSize(20);
+        let plane = this.add.image(-1500, 1080*.5, 'plane').setScale(2);
+
         this.input.on('pointerdown', () => {
-            this.cameras.main.fade(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('demo1'));
+            this.tweens.add({//text fade
+                targets: [clickText, menuText],
+                alpha: 0,
+                duration: 1000
+            })
+            this.tweens.add({//CraZy plAnE
+                targets: [plane],
+                x: 3200,
+                angle: 900,
+                delay: 1000,
+                duration: 3000
+            })
+
+            //Fade to black
+            this.time.delayedCall(1000, () => this.cameras.main.fade(1000, 0,0,0));
+            this.time.delayedCall(2000, () => this.scene.start('demo1'));
         });
     }
 }
